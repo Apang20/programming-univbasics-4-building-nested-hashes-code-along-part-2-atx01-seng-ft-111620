fc6e61864f110d4bcce40da375fdf0ae1f5b4fd3
convivial-parser-7775 <avelonpang@gmail.com>
2020-09-23 18:53:25 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -56,10 +56,10 @@ def add_characters
 
    # Write code below that does the following programmatically:
    
-   #epic_tragedy[:additional_characters] =>[{name: "Prince Escalus"}, {name: "Apothecary"}]
+   #epic_tragedy[:additional_characters] =>[{name: "Prince Escalus"}, {name: #"Apothecary"}]
    
-   epic_tragedy[:montague][:additional_characters] => {name: "Prince Escalus"}
-   epic_tragedy[:capulet][:additional_characters] => {name: "Apothecary"}
+   epic_tragedy[:montague][:additional_characters]{name: "Prince Escalus"}
+   epic_tragedy[:capulet][:additional_characters]{name: "Apothecary"}
    puts epic_tragedy
    
    # - Add an additional :additional_characters key to epic_tragedy and assign it an array as its value
